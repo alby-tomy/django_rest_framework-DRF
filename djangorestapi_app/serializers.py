4929017deeae6bc95b8b743d79f29c5d3766add5
alby-tomy <alby.u.tomy@gmail.com>
2024-08-02 14:44:50 +0530

    adding validate function in serilizer DRF

--- djangorestapi_app/serializers.py
+++ djangorestapi_app/serializers.py
@@ -7,4 +7,16 @@ class PersonSerializer(serializers.ModelSerializer):
         model = Person
         fields = '__all__'
         
-    
+    def validate(self,data):
+        special_char = "!@#$%^&*_+\-*;~"
+        
+        if any(c in special_char for c in data['name']):
+            raise serializers.ValidationError("Name should not contain any special characters")
+            
+        if not any(c in special_char for c in data['password']):
+            raise serializers.ValidationError(f"Password should contain at least one special character from [ {special_char} ]")
+            
+        if data['age'] < 18:
+            raise serializers.ValidationError("you are under 18")
+            
+        return data
